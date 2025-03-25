7cb6341f6690bb4a37143e191db9cb922321adb9
Huyen Tran <thtran.sg@gmail.com>
2025-03-25 22:59:13 +0100

    forth

--- exercise.py
+++ exercise.py
@@ -29,12 +29,59 @@
 # ket_qua = kiem_tra(so)
 # print(ket_qua)
 
-#baitapchanle_2
-so = float(input("Nhập số: "))
-if so.is_integer():  # Kiểm tra xem có phải số nguyên không
-    if int(so) % 2 == 0:
-        print("Số chẵn")
-    else:
-        print("Số lẻ")
-else:
-    print("Đây là số thực, không thể kiểm tra chẵn/lẻ!")
+# #baitapchanle_2
+# so = float(input("Nhập số: "))
+# if so.is_integer():  # Kiểm tra xem có phải số nguyên không
+#     if int(so) % 2 == 0:
+#         print("Số chẵn")
+#     else:
+#         print("Số lẻ")
+# else:
+#     print("Đây là số thực, không thể kiểm tra chẵn/lẻ!")
+# #baitapvesorted
+# list = [34,90,21,4454,22,1443,433]
+# list_sort = sorted(list)
+# print(list_sort)
+# #baitapthemsorted
+# danh_sach_hs_nu =['dao','thien','nhung','an','y']
+# print(sorted(danh_sach_hs_nu,key=len))
+# #baikhac
+# food =('ha', 'khoi bep','mien u dong dong','bun nam bo lau lau','pho ga ga','mien xao luon')
+# sorted_food = sorted(food)
+# print("Danh sach tang dan:",sorted_food) #tangdantheothutuAlphabe
+# sorted_food_desc = sorted (food,reverse=True)
+# print("Danh sach giam dan:",sorted_food_desc)#giamdantheothutuAlphabe
+# sorted_by_length= sorted(food,key=len) #dodaicuatu
+# print("Ds theo do dai ten mon:", sorted_by_length)
+#baitapkhac
+# magic = 'hocus,pocus'
+# magic1 = magic.replace('cus', 'key')
+# magic2 = magic.split(magic)
+# print(type(magic))
+# print(magic1)
+# # print(magic2)
+# non_la = {'nhu xe','hoc hanh','linh li','nhu xe', 'linh li'}
+# print(set(non_la))
+
+# #nummeric
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# #convert from int to float:
+# a = float(x)
+# #convert from float to int:
+# b = int(y)
+# #convert from int to complex:
+# c = complex(x)
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# #phanrandom
+# import random
+# colors = ['red', 'blue', 'green']
+# print(random.choice(colors))
